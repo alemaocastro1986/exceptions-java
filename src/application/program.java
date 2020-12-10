8512ca7787267b2cd71be53aa246e3d0d4ba7318
Andrius Cunha Castro <alemaocastro1986@gmail.com>
2020-12-10 16:03:55 -0300

    bad sollution

--- src/application/program.java
+++ src/application/program.java
@@ -36,22 +36,14 @@ public class program {
             System.out.print("Check-out: ");
             checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
+            String error = reservation.updateDates(checkIn, checkOut);
 
-            if(checkIn.before(now) || checkOut.before(now)){
-                System.out.println("Error in reservation: Reservation dates for update must be future date.");
-            }else if(!checkOut.after(checkIn)){
-                System.out.println("Error in reservation: Check-out date must be after check-in date.");
-            }else{
-                reservation.updateDates(checkIn, checkOut);
+            if (error != null) {
+                System.out.println("Error in reservation: " + error);
+            } else {
                 System.out.println("Reservation: " + reservation);
             }
-
-
-
-
         }
-
 
         sc.close();
 
